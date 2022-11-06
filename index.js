01796b548233bd6278715a9b80355ce289f98a00
Soham K <sohamk10@gmail.com>
2022-11-06 22:34:43 +0530

    Resolved merge conflict

--- index.js
+++ index.js
@@ -2,43 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const { getFiles, getFilesFromID } = require('./driveFunctions');
-
-const app = express();
-
-app.use(morgan('combined'));
-app.use(cors({ credentials: true, origin: true }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'go to /getfiles to check'
-  });
-});
-
-app.get('/getfiles', async (req, res) => {
-  let files = await getFiles();
-  res.json({
-    files
-  });
-});
-
-app.get('/getfilesfromid/:id', async (req, res) => {
-  console.log(req.params.id);
-  let files = await getFilesFromID(req.params.id);
-  res.json({
-    files
-  });
-});
-
-const port = process.env.port || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}.`));
-
-module.exports = app;
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const { getFiles, getFilesFromID, searchFor } = require('./driveFunctions');
 
 const app = express();
