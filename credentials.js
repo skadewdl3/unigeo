33c5e5672af2394b6d7c640462fd6c4d866583d1
Soham K <sohamk10@gmail.com>
2022-11-06 21:30:17 +0530

    commited changes before pulling

---
+++ credentials.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'service_account',
+  project_id: 'unigeo-366803',
+  private_key_id: '90ed3f549029aebe6499161950d45070ec91d33d',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC4aEQeE+jIdGNB\n/N7UTTSjGBKb7f14UNVfCx4Spja8rsNHLYcySBIjHDsrTYoJcV6y2tc+aF3lggLd\ninTqBudq4zuEI+AELd4GzgEVHv36G691gnLLWFpHE5zO/cnuPxdBIG8yo/6L1Ltt\nRYGlzyR6I2/pQvFq78niS6uGbnPsVxFAGsmok6kPJ5NDWFH6o8ATSTnCP/VLrfp6\nZBUIucQe3Q3yqdLCcmLXnkJhaVOZ0O/oH5IZGEcEVbFM7jpH2ZYfMFnmAMEjB7ag\nx/+1geUElxRK9eN3I9c8PZGz9cghwANlj+hHhW01hiDXsSYeiaWWL/7qka/0b/Tf\nJP8EM8XrAgMBAAECggEAA0qLxK79nZjuLemnHx+NoUeMYCS8PF8nn72hY3B8qPDE\nOCCo5A3YklsuTH84HSOyg/tXPMbnaMbhlDX6EN80rCSywDtVA94UXgdj345Ltstd\ns07eYPYKY8WxgRmTIWHA4X8EXQwDlAKk9wtMMrsNX0dYok95ODu1XQjqs8iENc3u\n6bh7llrurx7NxdU0zKVkoNwAZG+9YZftDhcI/P4xNciHmbVKxnaXScBYGMQPB7pv\nTN8/7xrd/oVVbPMKUuGVZ5nHUgrblpJ9UXwobj/jQc+V1kYUtuLGolBljtLuvzbU\nbvM2VdUslqSDDCHPmq3B832izF3SbPRQhnNnG0roIQKBgQDhf06lpZGMnr1sVTlb\n02uU0fJz+zrQMT0f+iBARhmmfy+EufwLC/Uj6uERRSwmROKNquQyQ7z4T9hmTQGs\nMRfp9uVPyTMRqYVeRxrebcPL5IQPyucMGA8OGJY7poOq092y01aGag0EHNNnZmYO\nDJ1vBPdkY1UjvtTSljmbGFExMQKBgQDRWhARTDbsDumg2E/ZXkItPiMQrfewJ/F6\nRQFsuEycR4gsAWb7Dc86nlHVvwqEj1bsIWN4H3MkYQegu66OZdw9FENlfd4uZUiW\nqN/aNKGuPSUdVfsSsPnAXW1wDJvdp7YuVJTg8X6XrplnX3NG8i5RlE16gBjYWcaV\nUE3CBwWB2wKBgFfzCGCxD/CxpR3Qq5t1/VrA+/hif1DeM4H6U4PeE3kN2CHDTFue\noVEN9hR/TzXp3hb1FSXJp7WNptBXrpRTXYliQ5P33FRCMwNPZlpko14t1Zg3QiBE\nbfLIgifnzSjek3SA2xl32XXGLGoC1UEbiHfZjwf3eiiD8IvmDbfBJ0GxAoGAALtE\n1RqkEn0W2eYHmWXX61qNOtCwzd/F2zyEfcHDGsSxOB4lIvlwYO8OsCWDvuTVU+vl\n8hSCDX19TlV5javTci8w8RjYfucBsu0SIywN6JL0Hz8eqnkN1ls9N6gDn2mnnE/V\nWVferl8A0DioKXZNLSHMh4INFCx0Ii9uph/qKAMCgYEAhDuyCb7MatcXClVdS6A8\niQ9pXe2Xs7tPM2ME7EbUEZmccFmXpXM3VpobsGbhObbM4ntc+8MhuaXHcuasIHiS\nQ539xz6PbxryQ566pRqqQQRZoXBNyBlEER/kiYwau9ZnhksVa6l462WAdGKjYqzd\nX4mz81Q305I3FAl/ooofOAA=\n-----END PRIVATE KEY-----\n',
+  client_email: 'unigeo@unigeo-366803.iam.gserviceaccount.com',
+  client_id: '113264884215506786937',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/unigeo%40unigeo-366803.iam.gserviceaccount.com'
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const { getFiles, getFilesFromID } = require('./driveFunctions');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(cors({ credentials: true, origin: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'go to /getfiles to check'
+  });
+});
+
+app.get('/getfiles', async (req, res) => {
+  let files = await getFiles();
+  res.json({
+    files
+  });
+});
+
+app.get('/getfilesfromid/:id', async (req, res) => {
+  console.log(req.params.id);
+  let files = await getFilesFromID(req.params.id);
+  res.json({
+    files
+  });
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}.`));
+
+module.exports = app;
